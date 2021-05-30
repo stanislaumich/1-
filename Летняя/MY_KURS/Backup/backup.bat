1f89048d1ce9a00497f274b3f81a4f235a85319b
stanislaumich <staslz42stas@gmail.com>
2021-05-30 15:54:49 +0300

    AUTO FROM WORK

--- Летняя/MY_KURS/Backup/backup.bat
+++ Летняя/MY_KURS/Backup/backup.bat
@@ -4,5 +4,5 @@ del backup.dmp
 sqlplus firma/firma @backup.sql
 exp firma/firma parfile=backup.dat
 del *.log
-rar a -sfx -m5 -ag_dd.mm.yyyy-hh-mi-ss backup @backup.lst
+rar a -m5 -ag_dd.mm.yyyy-hh-mi-ss backup @backup.lst
 rem pause
