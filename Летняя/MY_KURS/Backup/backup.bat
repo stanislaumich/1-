07b579ae34fefdd2342e9bb4b6f7aed21bb944e4
stanislaumich <staslz42stas@gmail.com>
2021-05-30 15:35:09 +0300

    AUTO FROM WORK

---
+++ Летняя/MY_KURS/Backup/backup.bat
@@ -0,0 +1,8 @@
+rem @echo off
+ren backup.dmp backup_prev.dmp
+del backup.dmp
+sqlplus firma/firma @backup.sql
+exp firma/firma parfile=backup.dat
+del *.log
+rar a -sfx -m5 -ag_dd.mm.yyyy-hh-mi-ss backup @backup.lst
+pause
