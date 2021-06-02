d1adbc3c5cbcca4b3fb7af9d3e8bfd9e43d8b1ea
stanislaumich <staslz42stas@gmail.com>
2021-06-02 07:41:10 +0300

    AUTO FROM WORK

--- Летняя/MY_KURS/Запросы.sql
+++ Летняя/MY_KURS/Запросы.sql
@@ -1,5 +1,5 @@
-/* Formatted on 01/06/2021 23:10:36 (QP5 v5.360) */
--- Кто из владельцев счетов данного банка имеет кредиты на  данную дату
+/* Formatted on 01/06/2021 23:48:24 (QP5 v5.360) */
+-- a Кто из владельцев счетов данного банка имеет кредиты на  данную дату
 
 SELECT name n, data d, summa s
   FROM schet s, firma9 f
@@ -14,14 +14,32 @@ SELECT fio n, data d, summa s
  WHERE f.idschet = s.id AND s.data >= '13/07/2006' AND s.credit = 'Y'
 --
 --
--- Определить юридический адрес и номера телефонов данной фирмы
+-- b Определить юридический адрес и номера телефонов данной фирмы
 
-/* Formatted on 01/06/2021 23:10:48 (QP5 v5.360) */
+
 SELECT uradr, phone FROM firma9
 
 --
--- Кто из сотрудников данной фирмы был принят на работу на данную дату
+-- c Кто из сотрудников данной фирмы был принят на работу на данную дату
 
-select *
-from sotrud
-where data='21/07/2018'
+/* Formatted on 01/06/2021 23:48:30 (QP5 v5.360) */
+SELECT *
+  FROM sotrud
+ WHERE data = '21/07/2018'
+
+--
+-- d Кто из поставщиков имеет кредит на сумму превышающую заданную?
+
+/* Formatted on 01/06/2021 23:50:50 (QP5 v5.360) */
+SELECT p.name, s.summa
+  FROM postav p, schet s
+ WHERE s.id = p.idschet AND s.Credit = 'Y' AND s.summa > 379
+ 
+ 
+ --
+ -- e Найти юридические адреса клиентов, которые оформили заказы на данную дату
+ 
+/* Formatted on 01/06/2021 23:54:15 (QP5 v5.360) */
+SELECT c.fio, c.adr
+  FROM client c, zakaz z
+ WHERE z.idclient = c.id AND z.data = '02/11/2020'
